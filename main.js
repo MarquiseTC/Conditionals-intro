03195b999271db413cc1b2df37f058faa8ed27bc
Marquise Campbell <marquisetyrrell@gmail.com>
2023-02-14 15:42:50 -0500

    Hope this worked

---
+++ main.js
@@ -0,0 +1,49 @@
+if(1 + 1 > 5){ console.log("True fact!");}
+else { console.log("Lies")}
+
+if(6-2 >= 0 ){ console.log("True fact!");}
+else { console.log("Lies")}
+
+if(4 * 5  <= 20){ console.log("True fact!");}
+else { console.log("Lies")}
+
+
+let likesDogs = "true"
+if(likesDogs = "True"){ console.log("You're a dog person!");}
+else { console.log("You're a cat person!")}
+
+
+const MarquiseObj = {Name: "Marquise",
+Language : "English"}
+
+const SarahObj = {Name: "Sarah",
+Language : "Spanish"}
+
+const TommyObj = {Name: "Tommy",
+Language : "French"}
+
+if(MarquiseObj){ console.log(`Hello ${MarquiseObj.Name}`);}
+else { console.log("Lies")};
+
+if(SarahObj){ console.log(`Hola ${SarahObj.Name}`);}
+else { console.log("Lies")};
+
+if(TommyObj){ console.log(`Bonjour ${TommyObj.Name}`);}
+else { console.log("Lies")};
+
+
+let grade = 76
+if (grade >= 0 && grade <= 69){console.log("You got an F");}
+else {console.log("Try again")}
+
+if (grade => 70 && grade <= 76){console.log("You got an D");}
+else {console.log("Try again")}
+
+if (grade >= 77 && grade <= 84){console.log("You got an C");}
+else {console.log("Try again")}
+
+if (grade >= 85 && grade <= 92 ){console.log("You got an B");}
+else {console.log("Try again")}
+
+if (grade >= 93 && grade <=100 ){console.log("You got an A");}
+else {console.log("Try again")}
